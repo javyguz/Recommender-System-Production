1bd3b41708073dc7cfcf0f953a79b67fbd6bd8ac
Javier Urrecha <javiergerardo7@gmail.com>
2022-08-27 15:13:40 -0500

    Added 1000 books

--- polls/template_tags/synopsis_algorithm.py
+++ polls/template_tags/synopsis_algorithm.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 
 
 # Function to recommend by content based
-def recommend(sg, data, title):
+def recommend(s, data, title):
+
     # Convert the index into series
     indices = pd.Series(data.index, index=data['title'])
 
@@ -10,24 +12,17 @@ def recommend(sg, data, title):
     try:
         idx = indices[title]
     except:
-        column_names = ['title', 'image', 'subjects']
+        column_names = ['title', 'image']
         empty_df = pd.DataFrame(columns=column_names)
         print("error, empty df")
-        return empty_df
 
-    # Get the pairwsie similarity scores
-    sig = list(enumerate(sg[idx]))
+    if type(idx) == np.int64:
+        movie_indices = s.loc[idx]
 
-    # Sort the books
-    sig = sorted(sig, key=lambda x: x[1], reverse=True)
-
-    # Scores of the 5 most similar books
-    sig = sig[1:6]
-
-    # Book indicies
-    movie_indices = [i[0] for i in sig]
+    else:
+        movie_indices = s.loc[idx[-1]]
 
     # Top 5 book recommendation
-    rec = pd.DataFrame(data[['title', 'image', 'subjects']].iloc[movie_indices])
+    rec = pd.DataFrame(data[['title', 'image_url']].iloc[movie_indices])
 
     return rec
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect # noqa: 401
-from django.shortcuts import get_object_or_404, render
-import numpy as np
+from django.shortcuts import render
 import pandas as pd
 import os
 from .template_tags.synopsis_algorithm import recommend
@@ -8,27 +7,10 @@ from .template_tags.synopsis_algorithm import recommend
 
 def index(request):
     if request.GET.get('mybtn'):
-        data_path = os.path.abspath("polls/static/tables/data_.csv")
-        data = pd.read_csv(data_path)
+        data = pd.read_csv(os.path.abspath("polls/static/tables/df_subset.csv"))
+        sg = pd.read_csv(os.path.abspath("polls/static/tables/sg.csv"), index_col=0)
 
-        sg_path_1 = os.path.abspath("polls/static/tables/sg1.npy")
-        sg1 = np.load(sg_path_1)
-
-        sg_path_2 = os.path.abspath("polls/static/tables/sg2.npy")
-        sg2 = np.load(sg_path_2)
-
-        sg_path_3 = os.path.abspath("polls/static/tables/sg3.npy")
-        sg3 = np.load(sg_path_3)
-
-        sg_path_4 = os.path.abspath("polls/static/tables/sg4.npy")
-        sg4 = np.load(sg_path_4)
-
-        sg_path_5 = os.path.abspath("polls/static/tables/sg5.npy")
-        sg5 = np.load(sg_path_5)
-
-        sg = np.concatenate([sg1, sg2, sg3, sg4, sg5])
-
-        context = {"Recommendation": list(recommend(sg, data, title=request.GET.get('mytextbox'))["image"]),
+        context = {"Recommendation": list(recommend(sg, data, title=request.GET.get('mytextbox'))["image_url"]),
                    "First": "false"}
 
         return render(request, 'pages/index.html', context)
